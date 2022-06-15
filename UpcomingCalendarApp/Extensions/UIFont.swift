240753fc302141c0dda69e9d18c59539ead0a1c1
Sima <sima_tatchenkova@mail.ru>
2022-06-15 12:36:58 +0300

    fonts are fixed

---
+++ UpcomingCalendarApp/Extensions/UIFont.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont.swift
+//  UpcomingCalendarApp
+//
+//  Created by Серафима  Татченкова  on 15.06.2022.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static let poppinsRegular16 = UIFont(name: "Poppins-Regular", size: 16)!
+    static let poppinsMedium16 = UIFont(name: "Poppins-Medium", size: 16)!
+}
